8862d27c576b940e5537e7c76964dd53be9c348c
zhou <diyuqianxiao@26.com>
2018-01-12 18:00:42 +0800

    加油站数据完善、基础增删改完成。

--- colltroller/gas/index.js
+++ colltroller/gas/index.js
@@ -15,23 +15,76 @@ gas_path.get('/',function (req,res) {
     res.render('gas/index')
 })
 gas_path.get('/api/find',function (req,res) {
-    let data = [
-        {
-            name:'1',
-            n2:'2',
-            n3:'3'
-        }
-    ];
-    res.json(data)
+    db.gasDB.find({}).sort({ updatedAt: -1 }).exec(function(err,docs){
+        res.json(docs)
+    })
+    
 })
-gas_path.get('/api/create',function(req,res){
+gas_path.post('/api/create',function(req,res){
     var form = new formidable.IncomingForm();
     form.parse(req,function(err,fields,files){
-        db.gasDB.insert(fields.data, function (err, newDocs) {
+        let gas = fields.gas;
+        gas.createdAt = moment().format();
+        gas.updatedAt = moment().format();
+        db.gasDB.insert(gas, function (err, newDocs) {
             res.status(200).json(newDocs)
         });
     })
-    
+})
+gas_path.post('/api/update',function(req,res){
+    var form = new formidable.IncomingForm();
+    form.parse(req,function(err,fields,files){
+        let gas = fields.gas;
+        gas.updatedAt = moment().format();
+        let id = gas._id;
+        delete gas._id;
+        db.gasDB.update({'_id':id},{$set:gas},{}, function (err, numReplaced) {
+            res.status(200).json(numReplaced)
+        });
+    })
+})
+gas_path.post('/api/updateDocFiled',function(req,res){
+    var form = new formidable.IncomingForm();
+    let numReplaced = 0;
+    form.parse(req,function(err,fields,files){
+        let dataGAS = fields.dataGAS;
+        for(let gas of dataGAS){
+            gas.updatedAt = moment().format();
+            let p92 = {
+                "discount":gas.discount92,
+                "date":gas.date
+            }
+            let p95 = {
+                "discount":'',
+                "date":''
+            }
+            let p98 = {
+                "discount":'',
+                "date":''
+            }
+            let p0 = {
+                "discount":'',
+                "date":''
+            }
+            gas.product={};
+            gas.product.p92=p92
+            gas.product.p95=p95
+            gas.product.p98=p98
+            gas.product.p0=p0
+            let id = gas._id;
+            delete gas._id;
+            db.gasDB.update({'_id':id},{$set:gas},{}, function (err, num) {
+                numReplaced=numReplaced+num;
+            });
+        }
+    })
+    res.status(200).json(numReplaced)
+})
+gas_path.get('/api/delete/:id',function(req,res){
+    let id = req.params.id;
+    db.gasDB.remove({_id:id},{}, function (err, numRemoved) {
+        res.status(200).json(numRemoved)
+    });
 })
 
 module.exports=gas_path;
